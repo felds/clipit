8b475b0e986d0bce175a3bf09fadabb2bc05ffe7
Felds Liscia <dev@felds.com.br>
2020-10-20 16:48:06 -0300

    load files

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import { useDropzone } from "react-dropzone";
+import "./styles.css";
 
-const App = () => <div>
-  Capotei
-</div>
+function App() {
+  return (
+    <div>
+      <MyDropzone />
+    </div>
+  );
+}
+
+type PlayerProps = {
+  file: File;
+};
+function Player({ file }: PlayerProps) {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  useEffect(() => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      // audioRef?.current.src = reader.result
+      audioRef.current!.src = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }, [file]);
+
+  // function onChange(event) {
+  //   var file = event.target.files[0];
+  //   var reader = new FileReader();
+  //   reader.onload = function(event) {
+  //     // The file's text will be printed here
+  //     console.log(event.target.result)
+  //   };
+
+  //   reader.readAsText(file);
+  // }
+
+  return (
+    <>
+      <audio controls ref={audioRef} autoPlay />
+    </>
+  );
+}
+
+function MyDropzone() {
+  const [file, setFile] = useState<File | null>(null);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    setFile(acceptedFiles[0]);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div>
+      <section className="drag" {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </section>
+
+      {file && (
+        <div className="player">
+          {file.name}
+          <hr />
+          <Player file={file} />
+        </div>
+      )}
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
