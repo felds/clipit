1e71362e4e2237ea6b85c486d122f3cac8b4102f
Felds Liscia <dev@felds.com.br>
2020-10-20 15:50:59 -0300

    meh.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => <div>
+  Capotei
+</div>
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +15,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// import * as serviceWorker from './serviceWorker';
+// serviceWorker.unregister();
