efd061b3861a1709e33745975e80fb84d49ee239
Felds Liscia <dev@felds.com.br>
2020-10-20 20:18:34 -0300

    chose where to start

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,22 +1,51 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 type PlayerProps = {
   file: File;
 };
 export default function Player({ file }: PlayerProps) {
+  const [duration, setDuration] = useState<number>(0);
+  const [start, setStart] = useState<number>(0);
+
   const audioRef = useRef<HTMLAudioElement>(null);
+
   useEffect(() => {
     const reader = new FileReader();
     reader.onload = () => {
       audioRef.current!.src = reader.result as string;
       audioRef.current!.volume = 0.01;
+      setStart(0);
     };
     reader.readAsDataURL(file);
   }, [file]);
 
+  function changeStart(e) {
+    const newStart = parseFloat((e.target as HTMLInputElement).value);
+    setStart(newStart);
+    audioRef.current!.currentTime = duration * newStart;
+    audioRef.current!.play();
+  }
+
+  function updateDuration(e) {
+    setDuration((e.target as HTMLAudioElement).duration);
+  }
+
   return (
     <div>
-      <audio src="" ref={audioRef} controls />
+      <audio src="" ref={audioRef} onDurationChange={updateDuration} controls />
+      <p>Duração do áudio: {duration.toFixed(2)} segundos</p>
+      <label>
+        Início
+        <input
+          type="range"
+          onMouseUp={changeStart}
+          min={0}
+          max={1}
+          step={0.01}
+          defaultValue={start}
+        />
+        {start.toFixed(2)}
+      </label>
     </div>
   );
 }
