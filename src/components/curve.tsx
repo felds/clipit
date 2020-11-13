10d8568e12047be00d5ab0d219b852d78d9f1c25
Felds Liscia <dev@felds.com.br>
2020-11-13 19:51:24 -0300

    process audio #WIP

--- src/components/curve.tsx
+++ src/components/curve.tsx
@@ -1,13 +1,11 @@
 import { area, curveBasis, scaleLinear, select } from "d3";
-import R from "ramda";
+import * as R from "ramda";
 import React, { useEffect, useRef, useState } from "react";
 
-const samples = 40;
-function randomArray() {
-  return Array(samples)
-    .fill(0)
-    .map((_) => (Math.random() * 10) >> 0);
-}
+console.log("shoesonaasd");
+
+const SAMPLES = 40;
+const MAX_CHANNELS = 2;
 
 const width = 800;
 const height = 200;
@@ -16,7 +14,7 @@ const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
 
 const xScale = scaleLinear()
-  .domain([0, samples - 1])
+  .domain([0, SAMPLES - 1])
   .range([0, innerWidth]);
 const yScale = scaleLinear().domain([0, 10]).range([innerHeight, 0]);
 const shape = area<number>()
@@ -33,35 +31,64 @@ const rms = R.compose(
   R.map((x: number) => x ** 2),
 );
 
-const processChannel = (channelData: number[]) =>
+const processChannel = (channelData: number[]): number[] =>
   R.compose(
     R.map(rms),
-    R.splitEvery(Math.ceil(R.length(channelData) / samples)),
+    R.splitEvery(Math.ceil(channelData.length / SAMPLES)),
   )(channelData);
 
-// ----------------------------------------------------------------
+// @todo send this process to a worker
+const loadFile = (file: File): Promise<ArrayBuffer> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = (e) => resolve(reader.result as ArrayBuffer);
+    reader.onerror = (e) => reject(new Error("Couldn't load file."));
+  });
 
+const getChannels = async (file: File): Promise<number[][]> => {
+  const channels: number[][] = [];
+  const audioContext = new AudioContext();
+  const buffer = await loadFile(file);
+  const audioData = await audioContext.decodeAudioData(buffer);
+  for (let i = 0; i < Math.min(audioData.numberOfChannels, MAX_CHANNELS); i++) {
+    channels.push(Array.from(audioData.getChannelData(i)));
+  }
+  return channels;
+};
+
+const loadAudioData = async (file: File): Promise<number[][]> => {
+  const audioData = await getChannels(file);
+  return audioData.map(processChannel);
+};
+
+// ----------------------------------------------------------------
 type CurveProps = {
   file: File;
 };
 export default function Curve({ file }: CurveProps) {
-  const [data, setData] = useState(randomArray());
+  const [data, setData] = useState();
   const pathRef = useRef<SVGPathElement>(null);
 
   useEffect(() => {
+    const initialData = R.times((_) => 1, SAMPLES);
     const path = select(pathRef.current!)
-      .datum(data)
+      .datum(initialData)
       .transition()
-      .attr("d", shape)
-      .end();
+      // @ts-ignore
+      .attr("d", shape);
   }, [data]);
 
   useEffect(() => {
     const path = select(pathRef.current).datum(data);
   }, [data]);
 
+  useEffect(() => {
+    console.log("Audio data", loadAudioData(file));
+  }, [file]);
+
   function shuffle() {
-    setData(randomArray());
+    console.log("XUFULE");
   }
 
   return (
